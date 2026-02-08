13823fe7079212e3ef2f2322d5c1220851e9d412
Sedima Mohale <sedimamohale18@gmail.com>
2026-02-08 14:08:42 +0200

    Set up React root rendering in main.jsx

--- @latest/src/main.jsx
+++ @latest/src/main.jsx
@@ -3,23 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { PodcastProvider } from "./context/PodcastContext";
-import { FavouritesProvider } from "./context/FavouritesContext";
-import { AudioProvider } from "./context/AudioContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-     
-    <PodcastProvider>
-      <AudioProvider>
-      <FavouritesProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    </FavouritesProvider>
-    </AudioProvider>
-    </PodcastProvider>
-    
-    
   </StrictMode>
 );
